af1e8959e49de5d4ca47010d379f61d0c42ab65e
Asonglefac Millenium Nzefe <asonglefacmillenium@gmail.com>
2021-05-24 13:24:25 +0100

    initialized repository

---
+++ index.js
@@ -0,0 +1,44 @@
+function getHistory() {
+    return document.getElementById('history-value').innerText;
+}
+
+function printHistory(num) {
+    document.getElementById('history-value').innerText = num;
+}
+
+function getOutput() {
+    return document.getElementById("output-value").innerText;
+}
+
+
+function printOutput(num) {
+
+    if (num == "") {
+        document.getElementById("output-value").innerText = num;
+    } else {
+
+        document.getElementById("output-value").innerText.getFormattedNumber(num);
+    }
+
+}
+
+
+function getFormattedNumber(num) {
+    var n = Number(num);
+    var value = n.toLocaleString('en');
+    return value;
+}
+
+
+
+function reverseNumberFormat(num) {
+    return Number(num.replace(/,/g, ''));
+}
+
+
+var operator = document.getElementsByClassName("operator");
+for (var i = 0; i < operator; i++) {
+    operator[i].addEventListener("click", function() {
+        alert("operator is" + this.id);
+    })
+}
